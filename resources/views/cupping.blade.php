64098531f6a285fa16ae28ec9581219bdb531f73
santos <srrvdwansem@gmail.com>
2024-11-06 13:13:16 +0100

    section

--- resources/views/cupping.blade.php
+++ resources/views/cupping.blade.php
@@ -1,5 +1,43 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1>Hello world!</h1>
+<section>
+    <h3>Hello world!</h3>
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perspiciatis, quas voluptatem. Ut voluptas eos adipisci non libero possimus ducimus odio. Quasi nihil et non, possimus culpa a, vitae harum praesentium porro obcaecati in? Distinctio atque fuga enim consequatur reiciendis alias corrupti provident quidem reprehenderit velit iste eligendi quam dicta, doloremque sequi fugit molestias aliquid saepe quod perspiciatis temporibus ab ratione sunt. Labore eos alias molestias ipsam eum recusandae laudantium aliquid architecto quis a, ab quae voluptatibus sequi odit blanditiis fugiat aliquam, quisquam perferendis dolore perspiciatis, excepturi assumenda cupiditate quidem. Blanditiis soluta est consequatur quasi! Velit ut sit ducimus quisquam architecto.</p>
+    <br>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dignissimos earum possimus, debitis cum eum minus eos soluta sunt necessitatibus, suscipit quaerat rem. Veniam, libero ipsum? Similique unde cupiditate a cumque molestiae aliquam ea porro quaerat voluptas saepe et temporibus mollitia consectetur voluptate id laudantium delectus, odio sequi fugiat labore culpa vitae quasi deleniti ipsam. Praesentium fugit a atque animi eius est dolorum necessitatibus inventore eaque nam fuga pariatur ipsa, laboriosam sequi aspernatur magni porro nobis nihil suscipit rem vitae cumque. Maiores facilis sint enim veniam autem vitae ex possimus cum esse, excepturi, nobis, tempora beatae? Est consequatur officiis quibusdam repellendus harum. Nesciunt modi error numquam hic officiis? Quod ipsam rem dolorum quo ipsa architecto corporis est dicta impedit. Amet, sit?</p>
+    <br>
+    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quis obcaecati nam fugiat laudantium fugit. Quia dicta voluptates voluptatem repellendus aspernatur.</p>
+</section>
+
+<section>
+    <h3>Tabel</h3>
+    <table border-collapse="collapse">
+        <tbody>
+            <tr>
+                <td>Row 1, Cell 1</td>
+                <td>Row 1, Cell 2</td>
+                <td>Row 1, Cell 3</td>
+                <td class="emphasize">Row 1, Cell 4</td>
+            </tr>
+            <tr>
+                <td>Row 2, Cell 1</td>
+                <td>Row 2, Cell 2</td>
+                <td>Row 2, Cell 3</td>
+                <td class="emphasize">Row 2, Cell 4</td>
+            </tr>
+            <tr>
+                <td>Row 3, Cell 1</td>
+                <td>Row 3, Cell 2</td>
+                <td>Row 3, Cell 3</td>
+                <td class="emphasize">Row 3, Cell 4</td>
+            </tr>
+            <tr>
+                <td colspan="3">Row 4, Cell 1</td>
+                <td class="emphasize">Row 4, Cell 4</td>
+            </tr>
+        </tbody>
+    </table>
+
+</section>
 @endsection
